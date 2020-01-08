da15d78d809a07bc83fd2284f918670e6e33ae0f
huangping1830 <huangping183@163.com>
2020-01-08 15:30:50 +0800

    更新1

--- CommomUtil/clear_log.py
+++ CommomUtil/clear_log.py
@@ -12,17 +12,18 @@ def Clear_log():
         if file.endswith('.log'):
             read_account(file)
     print("日志已清空！")
-
-# def Remove_log(N):
-#     file_list = get_all_file()
-#     for file in file_list:
-#         if file.endswith('.log'):
-#             f = os.path.split(file)[1]
-#             print(f)
-#             t = f[-14:-4]   #log名称
-#             print(t)
-#             if time.time()-StrToTimestamp(t) >= 24*60*60*int(N):
-#                 os.remove(file)
+    
+#暂无用
+def Remove_log(N):
+    file_list = get_all_file()
+    for file in file_list:
+        if file.endswith('.log'):
+            f = os.path.split(file)[1]
+            print(f)
+            t = f[-14:-4]   #log名称
+            print(t)
+            if time.time()-StrToTimestamp(t) >= 24*60*60*int(N):
+                os.remove(file)
 
 
 
@@ -33,13 +34,14 @@ def get_all_file(path = log_dir):
             file_list.append(os.path.join(cur_path,name))
     return file_list
 
-# def StrToTimestamp(Str=None,format='%Y %m %d'):
-#     if Str:
-#         timep = time.strptime(Str,format)
-#         res = time.mktime(timep)
-#     else:
-#         res=time.time()
-#     return int(res)
+#暂无用
+def StrToTimestamp(Str=None,format='%Y %m %d'):
+    if Str:
+        timep = time.strptime(Str,format)
+        res = time.mktime(timep)
+    else:
+        res=time.time()
+    return int(res)
 
 
 def read_account(filename):
